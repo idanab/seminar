e4bd4fef80d695b43306c0b33ca4d7aa34e9cd47
Idan Abrashkin <idan.abrashkin@mdclone.com>
2018-12-15 17:45:14 +0200

    adding venv

--- api.py
+++ api.py
@@ -1,6 +1,8 @@
+import ctypes
 import time
+from random import randint
 
-from flask import Flask
+from flask import Flask, request, jsonify
 from flask_limiter import Limiter
 from flask_limiter.util import get_remote_address
 
@@ -13,6 +15,26 @@ limiter = Limiter(
 @app.route("/test")
 def test():
     return "api is running!"
+
+
+@app.route('/random_nums', methods=['GET'])
+def get_random_nums():
+    size = int(request.args['size'])
+    return jsonify([randint(0, 1000) for _ in range(size)])
+
+
+@app.route('/execute', methods=['POST'])
+def execute_code():
+    json_request = request.get_json()
+    code = json_request['code']
+    is_admin = ctypes.windll.shell32.IsUserAnAdmin()
+    if is_admin:
+        return "Failed - could not eval expression because api is run by an administrator"
+    for expression in ["subprocess.", "shell", "rm ", "mv "]:
+        if expression in code:
+            return "Failed - the code you are trying to run might be malicious"
+    eval(code)
+    return "finished"
 
 
 @app.route("/denial_of_service")
